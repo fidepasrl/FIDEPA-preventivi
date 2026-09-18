86ce0a218d88e0d78b6493ae17d3f6f8b5ec6c34
Antonio <fidepasrl@gmail.com>
2026-09-18 18:16:11 +0200

    Versione 2.3.7

--- components/AppSidebar.tsx
+++ components/AppSidebar.tsx
@@ -307,7 +307,7 @@ export default function AppSidebar({
           )}
 
           <div className="px-1 text-[11px] text-[#2B2F5E]/55">
-            Versione 2.3.6 - Creato da Antonio Carbone
+            Versione 2.3.7 - Creato da Antonio Carbone
           </div>
         </div>
       </aside>
